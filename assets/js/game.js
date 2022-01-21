ca0c61b2a947a52df8aaf05e4e2de2fd0d09c2c6
DerekShearin <94636244+DerekShearin@users.noreply.github.com>
2022-01-21 18:43:56 -0500

    added option to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,8 @@
 
 //4: usage of curly brackets?
 
+//5: Define parts of the java code and their function
+
 //variables
 
 var playerName = window.prompt("What is  your robot's name?");
@@ -19,27 +21,23 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-  
 var fight = function() {
 
 // Alert players that they are starting the round
 window.alert("Welcome to Robot Gladiators!");
 };
+
 fight();
+
 //prompt player to fight or not to fight?
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
 //If player chooses to fight:
-if (promptFight === "fight" || promptFight === "FIGHT"); {
-  // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-  enemyHealth = enemyHealth - playerAttack;
-}
- if (enemyHealth)
+if (promptFight === "fight" || promptFight === "FIGHT"); { enemyHealth = enemyHealth - playerAttack;
+};
 
-
-// Log a resulting message to the console so we know that it worked.
 console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+ 
   //check enemy's health
 if (enemyHealth <= 0) {
   window.alert(enemyName + " has died!");
@@ -53,20 +51,28 @@ console.log(enemyName + " attacked " + playerName + ". " + playerName + " now ha
 
 //check player health:
 if (playerHealth <= 0) {
-  window.alert(playerName + "has died!");
- else { (playerHealth > 0) 
-  window.alert("Your player is still alive!");
+window.alert(playerName + " has died!");
+} else { window.alert(playerName +  " still has " + playerHealth + " health left.");
 }
-  //if player chooses to skip:
- else if (promptFight === "skip" || promptFight === "SKIP") {
-  window.alert(playerName + " has chosen to skip the fight.");
-  //confirmation player wants to skip fight:
-  var confirmSkip = windowConfirm("Are you sure you want to skip?") 
-  if (confirmSkip) {
-    window.alert(playerName + "has decided to skip this fight. Goodbye!");
-    playerMoney =playerMoney - 2;
-  }
-else { fight(); }
-else { 
-   window.alert("You need to choose a valid option. Try Again!");
- }
+
+// if player chooses to skip:  
+// else if (promptFight === "skip" || promptFight === "SKIP"); 
+//   window.alert(playerName + " has chosen to skip the fight.");
+//   var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+//   if (confirmSkip) {
+//       window.alert(playerName + " has decided to skip this fight. Goodbye!");
+//       playerMoney = playerMoney - 2; 
+// } else {
+//   window.alert("You need to choose a valid option. Try again!");
+//  }; else { 
+//    fight(); 
+//  }
+
+
+//  Game states:
+// "WIN" - player robot has defeated all enemy robots
+//  *fight all enrmy-robots
+//  * defeat all enemy-robots
+//  "LOSE" - Player robot's health is equal to zero or less than (playerHealth <= 0)
+  
